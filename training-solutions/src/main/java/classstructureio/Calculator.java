3350aa14ab955bcda5510737a534932f4903526d
tibo <tzjanosi@gamma.ttk.pte.hu>
2021-10-15 00:38:11 +0200

    calculator Done

---
+++ training-solutions/src/main/java/classstructureio/Calculator.java
@@ -0,0 +1,18 @@
+package classstructureio;
+
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("What is the first operand?");
+        int op1 = scanner.nextInt();
+
+        System.out.println("What is the second operand?");
+        int op2 = scanner.nextInt();
+
+        System.out.println(op1+" + " +op2+" = ");
+        System.out.println(op1+op2);
+    }
+}
