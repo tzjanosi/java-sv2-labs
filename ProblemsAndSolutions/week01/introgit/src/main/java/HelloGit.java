db72e7118609a66a6bf2e86afcc0a093258a710b
tibo <tzjanosi@gamma.ttk.pte.hu>
2021-10-14 23:59:25 +0200

    gitignore Done

---
+++ ProblemsAndSolutions/week01/introgit/src/main/java/HelloGit.java
@@ -0,0 +1,6 @@
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello IDEA and Git!");
+        System.out.println("Folder name has changed.");
+    }
+}
