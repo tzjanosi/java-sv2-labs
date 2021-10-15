35b0c88e59675ae6fab40a3b116f1e234b8e442e
tibo <tzjanosi@gamma.ttk.pte.hu>
2021-10-15 22:26:18 +0200

    Registration Done

---
+++ training-solutions/src/main/java/classstructureio/Registration.java
@@ -0,0 +1,19 @@
+package classstructureio;
+
+import java.util.Scanner;
+
+public class Registration {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Type in your name, please:");
+        String name = scanner.nextLine();
+
+        System.out.println("Type in xour email address, please:");
+        String emailCim = scanner.nextLine();
+
+        System.out.println("The data, provided during the registration are the following:");
+        System.out.println("Name: "+name);
+        System.out.println("Email Address: "+emailCim);
+    }
+}
