d8f953f45c9ecd221ccb175ec23d85ea02bdb3cd
tibo <tzjanosi@gamma.ttk.pte.hu>
2021-10-21 21:31:15 +0200

    "Háromszögmátrix" Done

--- training-solutions/src/main/java/arrayofarrays/Triangular.java
+++ training-solutions/src/main/java/arrayofarrays/Triangular.java
@@ -1,4 +1,32 @@
 package arrayofarrays;
 
+import java.util.Scanner;
+
 public class Triangular {
+    int[][] triangularMatrix(int size){
+        int[] innerArray;
+        int[][] triangleArray=new int[size][];
+        for (int i = 0; i < size; i++) {
+            innerArray= new int[i+1];
+            for (int j = 0; j < innerArray.length; j++) {
+                innerArray[j]=i;
+            }
+            triangleArray[i]=innerArray;
+        }
+        return triangleArray;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        Triangular triangular=new Triangular();
+        ArrayOfArrays arrayOfArrays=new ArrayOfArrays();
+
+        System.out.println("Type in the number of rows in the triangular matrix:");
+        int rowNumber=scanner.nextInt();
+
+        int[][] triangularMatrix=triangular.triangularMatrix(rowNumber);
+        arrayOfArrays.printArrayOfArrays(triangularMatrix);
+
+    }
 }
