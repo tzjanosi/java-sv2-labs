3ed9efc3017c462e58f6129bf6c8251d0a8387a4
tibo <tzjanosi@gamma.ttk.pte.hu>
2021-10-21 22:36:09 +0200

    "Szorzótábla" Done

--- training-solutions/src/main/java/arrays/ArraysMain.java
+++ training-solutions/src/main/java/arrays/ArraysMain.java
@@ -2,6 +2,7 @@ package arrays;
 
 import java.time.YearMonth;
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class ArraysMain {
     String numberOfDaysAsString(){
@@ -13,9 +14,35 @@ public class ArraysMain {
         }
         return Arrays.toString(numberOfDays);
     }
+    void multiplicationTableAsString(int size){
+        int[] innerArray;
+        int[][] multiplicationTable=new int[size+1][size+1];
+        int multiplicatorI;
+        int multiplicatorJ;
+        for (int i = 0; i < size+1; i++) {
+            innerArray= new int[size+1];
+            multiplicatorI=(i==0?1:i);
+            for (int j = 0; j < size+1; j++) {
+                multiplicatorJ=(j==0?1:j);
+                innerArray[j]=multiplicatorI*multiplicatorJ;
+            }
+            if(i==0){
+                innerArray[0]=0;
+            }
+            multiplicationTable[i]=innerArray;
+            System.out.println(Arrays.toString(innerArray));
+        }
+    }
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         ArraysMain arraysMain=new ArraysMain();
         System.out.println(arraysMain.numberOfDaysAsString());
+
+        System.out.println();
+        System.out.println("Type in the size of multiplication table:");
+        int size=scanner.nextInt();
+        arraysMain.multiplicationTableAsString(size);
     }
 }
