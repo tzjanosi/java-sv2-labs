11f903d89cd21f701f0b041298eb1af292ac030e
tibo <tzjanosi@gamma.ttk.pte.hu>
2021-10-21 15:13:15 +0200

    "Tömbök tömbjének kiírása" Done

---
+++ training-solutions/src/main/java/arrayofarrays/ArrayOfArrays.java
@@ -0,0 +1,18 @@
+package arrayofarrays;
+
+public class ArrayOfArrays {
+    public static void printArrayOfArrays(int[][] a){
+        for (int i = 0; i < a.length; i++) {
+            int[] innerArray=a[i];
+            for (int j = 0; j < innerArray.length; j++) {
+                System.out.print(innerArray[j]+" ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        int[][] numbers = {{4,3,2,1}, {1, 2,3}, {1, 2}, {2, 4, 6}, {2, 4, 8, 16}};
+        printArrayOfArrays(numbers);
+    }
+}
