9d6357626cd8a032e19790a6c6b7b0f72fffe564
tibo <tzjanosi@gamma.ttk.pte.hu>
2021-10-22 12:56:06 +0200

    "Kedvencek listája" Done

---
+++ training-solutions/src/main/java/aslist/Favourites.java
@@ -0,0 +1,28 @@
+package aslist;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Favourites {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        List<String> favouriteThings = new ArrayList<>();
+
+        System.out.println("Type in your favorite movie:");
+        String favThing=scanner.nextLine();
+        favouriteThings.add(favThing);
+
+        System.out.println("Type in your favorite book:");
+        favThing=scanner.nextLine();
+        favouriteThings.add(favThing);
+
+        System.out.println("Type in your favorite animal:");
+        favThing=scanner.nextLine();
+        favouriteThings.add(favThing);
+        System.out.println("Thr size of the list is: "+favouriteThings.size());
+        System.out.println(favouriteThings);
+
+
+    }
+}
