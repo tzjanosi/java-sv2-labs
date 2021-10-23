a982a4d9bcd67c9ac5bc59f3ce8412fb5e6bd8a6
tibo <tzjanosi@gamma.ttk.pte.hu>
2021-10-23 23:20:34 +0200

    "Két szerencsés ember" Done

---
+++ training-solutions/src/main/java/math/math/MathMethods.java
@@ -0,0 +1,8 @@
+package math.math;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MathMethods {
+
+}
---
+++ training-solutions/src/main/java/math/random/RandomDraw.java
@@ -0,0 +1,17 @@
+package math.random;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
+public class RandomDraw {
+    public static void main(String[] args) {
+        Random rnd = new Random();
+        String[] names = {"Monday Joe","Tuesday Jack","Wednesday David","Thursday Mary","Friday Ivan","Saturday Alex","Sunday Robert","White lilly","Black Jack","Never Enough"};
+        List<String> namesList = Arrays.asList(names);
+        System.out.println("The candidates are: "+namesList);
+        int winnwer1 = rnd.nextInt(5);
+        int winnwer2 = 5+rnd.nextInt(5);
+        System.out.println("And the winners are: "+namesList.get(winnwer1)+" and "+namesList.get(winnwer2));
+    }
+}
