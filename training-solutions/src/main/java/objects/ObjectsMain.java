a60f3aff82a8e792d4860bffb8a2432744ff5706
tibo <tzjanosi@gamma.ttk.pte.hu>
2021-10-24 07:09:26 +0200

    "Objektumok száma" Done

--- training-solutions/src/main/java/objects/ObjectsMain.java
+++ training-solutions/src/main/java/objects/ObjectsMain.java
@@ -25,7 +25,32 @@ public class ObjectsMain {
         System.out.println("Are identical book and anotherBook: "+(book==anotherBook));
         anotherBook=book;
         System.out.println("Are identical book and anotherBook: "+(book==anotherBook));
+        System.out.println();
+        System.out.println();
 
+        Book book1 = new Book();
+        Book book2 = new Book();
+        Book book3 = new Book();
+        Book book4 = book1;
+        Book book5 = book1;
+        Book book6 = book3;
+        Book book7 = null;
+        book4 = book6;
+        new Book();
+        book5 = new Book();
+        book6 = null;
+        book5 = book4;
+
+        System.out.println();
+        System.out.println("A létrehozott objektumok száma: 5");
+        System.out.println(book1);
+        System.out.println(book2);
+        System.out.println(book3);
+        System.out.println(book4);
+        System.out.println(book5);
+        System.out.println(book6);
+        System.out.println(book7);
+        System.out.println("A hozzáférhető objektumok száma: 3");
 
     }
 }
