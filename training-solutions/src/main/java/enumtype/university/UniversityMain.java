cc8e1c6a5ad29fbbf02ba624984826ea5850049a
tibo <tzjanosi@gamma.ttk.pte.hu>
2021-10-28 23:11:18 +0200

    "Egyetemek" Done

--- training-solutions/src/main/java/enumtype/university/UniversityMain.java
+++ training-solutions/src/main/java/enumtype/university/UniversityMain.java
@@ -1,7 +1,5 @@
 package enumtype.university;
 
-import enumtype.solarsystem.SolarSystem;
-
 public class UniversityMain {
     public static void main(String[] args) {
         for(University university:University.values()){
