f2c1fa86f196007844187d1fef565c54533d1c92
tibo <tzjanosi@gamma.ttk.pte.hu>
2021-11-14 21:01:36 +0100

    "Újabb három tömb" Done

---
+++ training-solutions/src/main/java/introexceptionmoreexceptions/Three.java
@@ -0,0 +1,22 @@
+package introexceptionmoreexceptions;
+
+import java.util.Arrays;
+
+public class Three {
+    public static void main(String[] args) {
+        int[] first = new int[]{3, 7, 24, 0, 466, 8, 0 ,42, 2, 6, 0};
+        int[] second = new int[]{676, 4, 31, 5, 2, 432, 643, 0, 1, 42, 456};
+        int[] third = new int[second.length];
+
+        for (int i = 0; i < second.length; i++) {
+            if(i == 7){
+                System.out.println("A 'second' tömb 8-ik eleme nulla és azzal kellene osztani.");
+                System.out.println("Ezért kapunk 'ArithmeticException: / by zero' hibát");
+            }
+            third[i] = first[i] / second[i];
+            System.out.println(third[i]);
+        }
+
+        System.out.println(Arrays.toString(third));
+    }
+}
