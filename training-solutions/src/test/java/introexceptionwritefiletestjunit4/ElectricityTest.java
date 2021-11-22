9083420f1cebb75620e9e132354d0e90e18aff88
tibo <tzjanosi@gamma.ttk.pte.hu>
2021-11-22 20:23:55 +0100

    "Áramszünet" Done

---
+++ training-solutions/src/test/java/introexceptionwritefiletestjunit4/ElectricityTest.java
@@ -0,0 +1,28 @@
+package introexceptionwritefiletestjunit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class ElectricityTest {
+    @Rule
+    public TemporaryFolder temporaryFolder = new TemporaryFolder();
+
+    @Test
+    public void writeNames() throws IOException {
+        Path path = temporaryFolder.newFile("streets.txt").toPath();
+
+        new Electricity().writeStreets(path);
+
+        List<String> lines = Files.readAllLines(path);
+        assertEquals(Arrays.asList("2021. 11. 22.","Ádám", "Éva", "Tündér","Jonatán"), lines);
+    }
+}
