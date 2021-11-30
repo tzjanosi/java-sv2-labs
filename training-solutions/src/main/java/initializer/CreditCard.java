73060d022567afc2aab78240101fd9c8436927cf
tibo <tzjanosi@gamma.ttk.pte.hu>
2021-11-30 13:06:51 +0100

    "Hitelkártya használat" Done

--- training-solutions/src/main/java/initializer/CreditCard.java
+++ training-solutions/src/main/java/initializer/CreditCard.java
@@ -1,6 +1,5 @@
 package initializer;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
--- training-solutions/src/test/java/initializer/CreditCardTest.java
+++ training-solutions/src/test/java/initializer/CreditCardTest.java
@@ -2,8 +2,6 @@ package initializer;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class CreditCardTest {
