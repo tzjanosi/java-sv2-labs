c07ca97919f1dc59264006d8af8eef29cadc06b3
tibo <tzjanosi@gamma.ttk.pte.hu>
2021-12-01 08:42:05 +0100

    "Konstruktorok hívási lánca" Done

---
+++ training-solutions/src/main/java/inheritanceconstructor/animal/AnimalMain.java
@@ -0,0 +1,10 @@
+package inheritanceconstructor.animal;
+
+public class AnimalMain {
+    public static void main(String[] args) {
+        Sparrow sparrow=new Sparrow();
+
+        System.out.println();
+        System.out.println("Elöbb az ősosztály konstruktorát hívja meg.");
+    }
+}
