8c02a74b1eee7c25d2aa4d8e33d34facea67431f
tibo <tzjanosi@gamma.ttk.pte.hu>
2021-12-08 14:08:06 +0100

    "Személykocsi, teherkocsi, komphajó" Done

---
+++ training-solutions/src/main/java/virtualmethod/vehicle/FerryBoat.java
@@ -0,0 +1,32 @@
+package virtualmethod.vehicle;
+
+public class FerryBoat extends Vehicle{
+    private static final int MAX_CARRY_WEIGHT=2000;
+    private Car car=null;
+
+    public FerryBoat(int vehicleWeight) {
+        super(vehicleWeight);
+    }
+
+    @Override
+    public int getGrossLoad() {
+        return super.getGrossLoad()+car.getGrossLoad();
+    }
+    public boolean canCarry(Car car){
+        return car.getGrossLoad()<MAX_CARRY_WEIGHT;
+    }
+    public boolean load(Car car){
+        if (canCarry(car)){
+            this.car=car;
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return "FerryBoat{" +
+                "car=" + car.toString() +
+                '}';
+    }
+}
---
+++ training-solutions/src/main/java/virtualmethod/vehicle/Vehicle.java
@@ -0,0 +1,24 @@
+package virtualmethod.vehicle;
+
+public class Vehicle {
+    private int vehicleWeight;
+    protected static final int PERSON_AVERAGE_WEIGHT=75;
+
+    public Vehicle(int vehicleWeight) {
+        this.vehicleWeight = vehicleWeight;
+    }
+    public int getGrossLoad(){
+        return vehicleWeight+PERSON_AVERAGE_WEIGHT;
+    }
+
+    @Override
+    public String toString() {
+        return "Vehicle{" +
+                "vehicleWeight=" + vehicleWeight +
+                '}';
+    }
+
+    public int getVehicleWeight() {
+        return vehicleWeight;
+    }
+}
