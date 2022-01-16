0dca194a4178eb4fb37c6143c2bcc7a8abf39ca9
tibo <tzjanosi@gamma.ttk.pte.hu>
2022-01-16 22:01:44 +0100

    "Árbevételek" is Done

---
+++ training-solutions/src/main/java/sorting/Income.java
@@ -0,0 +1,16 @@
+package sorting;
+
+import java.util.Arrays;
+
+public class Income {
+    private int[] incomes;
+
+    public Income(int[] incomes) {
+        this.incomes = incomes;
+    }
+
+    public int getHighestIncome(){
+        Arrays.sort(incomes);
+        return incomes[incomes.length-1];
+    }
+}
